96417429b505a24e5d88f60625a6ce5e11d8c7a9
Leo Wilson <leowilson.xlw123@gmail.com>
2018-10-10 13:25:00 -0700

    Finish code

--- transLoop.py
+++ transLoop.py
@@ -19,15 +19,41 @@ def getLang(langs, prev):
     l = random.choice(langs)
     if(l == prev):
         l = getLang(langs, prev)
-    return l;
+    return l
 def genPath(langs, len, start, end):
-    p = start;
+    p = start
     for i in range(1, len-2):
-        p += '\n' + random.choice(langs)
+        p += '\n' + getLang()
     p += '\n' + end
+    return p
 def t(f, t, s):
     return getJSON(u = 'https://translate.googleapis.com/translate_a/single', p = {'client':'gtx', 'sl':f, 'tl':t, 'dt':'t', 'q':s})[0][0][0]
+def follow(path, txt):
+    path = path.split('\n')
+    prev = path[0]
+    for(i in range(1, len(path))):
+        txt = t(prev, path[i], txt)
+        prev = path[i]
+def loadCFG(fname):
+    return json.loads(r(fname))
+def transLoop(cfg):
+    txt = r(cfg['input'])
 
-cfg = json.loads(r('res/cfg.json')) # change the file path if something else is used
-txt = r(cfg['input'])
-c = cfg['startingLang']
+    if(cfg['useDefinedPathIfAvail'] and r(cfg['path']) != ''):
+        w(cfg['output'], follow(r(cfg['path']), txt))
+    else:
+        path = genPath(r(cfg['genPath']['langs']), cfg['genPath']['length'], cfg['genPath']['start'], cfg['genPath']['end'])
+        w(cfg['path'], path)
+        w(cfg['output'], follow(path, txt))
+
+def main():
+    cfg = loadCFG('res/cfg.json') # change the file path if something else is used
+    if(cfg['mode'] == 'transLoop'):
+        transLoop(cfg)
+    elif(cfg['mode'] == 'genPath'):
+        w(cfg['path'], genPath(r(cfg['genPath']['langs']), cfg['genPath']['length'], cfg['genPath']['start'], cfg['genPath']['end']))
+    else:
+        print('[ERROR] Invalid mode')
+
+if __name__ == '__main__':
+    main()
